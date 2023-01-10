c2287affd03c874f459f506b9d9c63028c92a511
BJ Smith <bryanjeremysmith@gmail.com>
2023-01-10 15:33:06 -0800

    handle get notes and added server.js for routes

---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// GET /notes should return the notes.html file.
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// GET /api/notes returns notes
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+// GET * should return the index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
