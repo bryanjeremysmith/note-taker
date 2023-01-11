1b0fb5c5a12de01ba121c825a0d13cee6a15ed89
BJ Smith <bryanjeremysmith@gmail.com>
2023-01-10 16:09:52 -0800

    updated db.json contents, added uuid helper, handling post

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
+
 const PORT = 3001;
 
 const app = express();
@@ -18,6 +21,59 @@ app.get('/notes', (req, res) => {
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, 
+// and then return the new note to the client. You'll need to find a way to give each note a unique id when saved.
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+        title,
+        text,
+        note_id: uuid()
+    };
+
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+  
+        // Add a new review
+        parsedNotes.push(newNote);
+  
+        // Write updated reviews back to the file
+        fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+    });
+
+    const response = {
+        status: 'success',
+        body: newNote,
+    };
+  
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
 });
 
 // GET * should return the index.html file.
